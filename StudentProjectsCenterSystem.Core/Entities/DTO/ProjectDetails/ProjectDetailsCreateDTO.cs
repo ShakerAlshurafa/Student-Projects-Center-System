9df1e11b70e8d15ba748275c532d48a2dd5e989e
Shaker Yousef <shakrshrfa@gmail.com>
2024-11-11 10:10:22 +0200

    Refactor Create Project Details endpoint to support multiple details entries and improve validation

--- StudentProjectsCenterSystem.Core/Entities/DTO/ProjectDetails/ProjectDetailsCreateDTO.cs
+++ StudentProjectsCenterSystem.Core/Entities/DTO/ProjectDetails/ProjectDetailsCreateDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,13 @@ namespace StudentProjectsCenterSystem.Core.Entities.DTO.MyProject
 {
     public class ProjectDetailsCreateDTO
     {
+        [Required(ErrorMessage = "Title is required.")]
+        [MaxLength(100, ErrorMessage = "Title cannot exceed 100 characters.")]
         public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Description is required.")]
+        [MaxLength(500, ErrorMessage = "Description cannot exceed 500 characters.")]
         public string Description { get; set; } = string.Empty;
-        public int SectionId { get; set; }
         public byte[]? IconData { get; set; } = Array.Empty<byte>();
     }
 }
--- StudentProjectsCenterSystem.Core/Entities/Domain/project/ProjectDetails.cs
+++ StudentProjectsCenterSystem.Core/Entities/Domain/project/ProjectDetails.cs
@@ -1,4 +1,5 @@
 ﻿using StudentProjectsCenterSystem.Core.Entities.Domain.project;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace StudentProjectsCenterSystem.Core.Entities.project
@@ -6,7 +7,13 @@ namespace StudentProjectsCenterSystem.Core.Entities.project
     public class ProjectDetailEntity
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
+        [MaxLength(100, ErrorMessage = "Title cannot exceed 100 characters.")]
         public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Description is required.")]
+        [MaxLength(500, ErrorMessage = "Description cannot exceed 500 characters.")]
         public string Description { get; set; } = string.Empty;
         public byte[]? IconData { get; set; } = Array.Empty<byte>();   // Icon as binary data
 
