3d878f5e494f16fc02b568f9c780cc487bd0e888
Shaker Yousef <shakrshrfa@gmail.com>
2024-11-19 11:29:02 +0200

    Enhance 'GetById' endpoint and add 'GetAll' endpoint in WorkgroupController

--- StudentProjectCenterSystem/Controllers/WorkgroupController.cs
+++ StudentProjectCenterSystem/Controllers/WorkgroupController.cs
@@ -1,10 +1,15 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using StudentProjectsCenter.Core.Entities.DTO.Workgroup;
 using StudentProjectsCenterSystem.Core.Entities;
+using StudentProjectsCenterSystem.Core.Entities.Domain.project;
 using StudentProjectsCenterSystem.Core.Entities.Domain.workgroup;
 using StudentProjectsCenterSystem.Core.Entities.DTO.Workgroup;
+using StudentProjectsCenterSystem.Core.Entities.project;
 using StudentProjectsCenterSystem.Core.IRepositories;
+using StudentProjectsCenterSystem.Infrastructure.Repositories;
 using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
 
 namespace StudentProjectsCenterSystem.Controllers
 {
@@ -21,31 +26,73 @@ namespace StudentProjectsCenterSystem.Controllers
             this.mapper = mapper;
         }
 
-        [HttpGet]
-        public async Task<ActionResult<ApiResponse>> GetById([Required,FromQuery] int workgroupId)
+
+        [HttpGet("get-all")]
+        public async Task<ActionResult<ApiResponse>> GetAll(
+            [FromQuery] string? workgroupName = null, 
+            [FromQuery] int PageSize = 6, 
+            [FromQuery] int PageNumber = 1)
+        {
+            Expression<Func<Workgroup, bool>> filter = x => true;
+            if (!string.IsNullOrEmpty(workgroupName))
+            {
+                filter = x => x.Name.Contains(workgroupName);
+            }
+            var model = await unitOfWork.workgroupRepository.GetAll(filter, PageSize, PageNumber, "Project.UserProjects.User");
+
+            if (!model.Any())
+            {
+                return new ApiResponse(200, "No Workgroups Found");
+            }
+
+            var viewModel = model.Select(w =>
+            {
+                var userProjects = w?.Project?.UserProjects ?? new List<UserProject>();
+                return new AllWorkgroupsDTO
+                {
+                    Id = w.Id,
+                    Name = w.Name,
+                    SupervisorName = userProjects
+                                        .FirstOrDefault(u => u.Role == "Supervisor")?.User.UserName ?? string.Empty,
+                    CoSupervisorName = userProjects
+                                        .FirstOrDefault(u => u.Role == "Co-Supervisor")?.User.UserName ?? string.Empty,
+                    CustomerName = userProjects
+                                        .FirstOrDefault(u => u.Role == "Customer")?.User.UserName ?? string.Empty,
+                    Company = userProjects
+                                        .FirstOrDefault(u => u.Role == "Customer")?.User.CompanyName ?? string.Empty,
+                };
+            });
+
+            return Ok(new ApiResponse(200, "Workgroups retrieved successfully", viewModel));
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ApiResponse>> GetById([Required] int id)
         {
             // Retrieve the workgroup with related data for Project, UserProjects, and associated users.
-            var workgroup = await unitOfWork.workgroupRepository.GetById(workgroupId, includeProperty: "Project.UserProjects.User");
+            var workgroup = await unitOfWork.workgroupRepository.GetById(id, includeProperty: "Project.UserProjects.User");
 
             if (workgroup == null)
             {
                 return NotFound(new ApiResponse(404, "Workgroup not found."));
             }
 
+            var userProjects = workgroup.Project?.UserProjects ?? new List<UserProject>();
             var workgroupDto = new WorkgroupDTO
             {
                 Id = workgroup.Id,
                 Name = workgroup.Name,
                 Progress = workgroup.Progress,
-                SupervisorName = workgroup.Project?.UserProjects
+                SupervisorName = userProjects
                                 .FirstOrDefault(u => u.Role == "Supervisor")?.User.UserName ?? string.Empty,
-                CoSupervisorName = workgroup.Project?.UserProjects
+                CoSupervisorName = userProjects
                                 .FirstOrDefault(u => u.Role == "Co-Supervisor")?.User.UserName,
-                CustomerName = workgroup.Project?.UserProjects
+                CustomerName = userProjects
                                 .FirstOrDefault(u => u.Role == "Customer")?.User.UserName ?? string.Empty,
-                Company = workgroup.Project?.UserProjects
+                Company = userProjects
                                 .FirstOrDefault(u => u.Role == "Customer")?.User.CompanyName ?? string.Empty,
-                Team = workgroup?.Project?.UserProjects?
+                Team = userProjects
                         .Where(u => u.Role.ToLower() == "student" && u.User?.UserName != null)
                         .Select(u => u.User!.UserName!)  
                         .ToList() ?? new List<string>()
@@ -54,6 +101,7 @@ namespace StudentProjectsCenterSystem.Controllers
 
             return Ok(new ApiResponse(200, "Workgroup retrieved successfully", workgroupDto));
         }
+
 
     }
 }
